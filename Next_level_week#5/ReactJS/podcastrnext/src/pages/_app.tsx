7d78038b448edd544ba662c12ace01368e06f299
Silas Teles Goes <42805518+silastgoes@users.noreply.github.com>
2021-04-21 19:48:09 -0300

    Class 2
    Header, Player, Server and Testing Api

--- Next_level_week#5/ReactJS/podcastrnext/src/pages/_app.tsx
+++ Next_level_week#5/ReactJS/podcastrnext/src/pages/_app.tsx
@@ -1,5 +1,19 @@
+import { Header } from '../conponents/Header'
+
+import '../styles/global.scss'
+import styles from '../styles/app.module.scss'
+import { Player } from '../conponents/Player';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header/>
+        <Component {...pageProps} />
+      </main>
+      <Player/>
+    </div>
+  );
 }
 
 export default MyApp
